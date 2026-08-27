cc10132def5438f7a0c6bcf059c2a7db84391247
Ange Marquise <angemarquisei@gmail.com>
2026-08-27 23:15:51 +0200

    components

---
+++ components.jsx
@@ -0,0 +1,19 @@
+<body>
+    //react components (components  are basically a reusable piece of your user interface.)
+    <MyAwesomeNavbar />
+    <MainContent />
+    <MyAwesomeFooter />
+    </body>
+
+    //react is Composable
+
+//Composable means: You can build something bigger by combining smaller reusable pieces.
+
+/*
+  Challenge:
+  Create your very first custom React component!
+  Call it "MainContent", and have it return a simple
+  h1 element that says "React is great!"
+  Afterward, render it on the line below MyAwesomeNavbar
+ */
+//solution
