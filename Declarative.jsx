269756d98c0332ac20c111ec670681da5f195bfb
Ange Marquise <angemarquisei@gmail.com>
2026-08-29 22:47:42 +0200

    declarative and imperative

---
+++ Declarative.jsx
@@ -0,0 +1,22 @@
+//Declarative : Declarative programming means you describe the result you want, and the system figures out the steps needed to achieve it.
+//Imperative : Imperative programming means you give the computer the specific steps it needs to follow to accomplish something.
+
+// import { createRoot } from "react-dom/client"
+// const root = createRoot(document.getElementById("root"))
+
+// root.render(
+//     <h1>Hello, React!</h1>
+// )
+
+
+/* 
+Challenge - recreate the above line of code in vanilla JS by creating and
+appending an h1 to our div#root (without using innerHTML).
+
+- Create a new h1 element
+- Give it some textContent
+- Give it a class name of "header"
+- append it as a child (using `appendChild`) of the div#root
+    
+*/
+
